5f536b3fba3c3f011d85cc6b17257c031a985d26
Almadavic <almadavic@live.com>
2022-08-05 11:26:01 -0300

    Project Created

---
+++ src/main/java/application/DrawTeamApplication.java
@@ -0,0 +1,13 @@
+package application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrawTeamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrawTeamApplication.class, args);
+	}
+
+}
