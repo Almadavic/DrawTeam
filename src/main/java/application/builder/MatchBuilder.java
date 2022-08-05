b8b76b17b1d9c91f96afaac65786ba8a0761ed01
Almadavic <almadavic@live.com>
2022-08-05 12:15:43 -0300

    MatchBuilder and MatchRepository created

---
+++ src/main/java/application/builder/MatchBuilder.java
@@ -0,0 +1,37 @@
+package application.builder;
+
+import application.entity.Match;
+import application.entity.Team;
+import lombok.Getter;
+
+@Getter
+public class MatchBuilder { // Constroe um Match de uma maneira mais agradável e legivel.
+
+    private Integer numberPlayers;
+
+    private Team teamA;
+
+    private Team teamB;
+
+    public MatchBuilder setNumberPlayers(int numberPlayers) {
+        this.numberPlayers = numberPlayers;
+        return this;
+    }
+
+    public MatchBuilder setTeamA(Team teamA) {
+        this.teamA = teamA;
+        return this;
+    }
+
+    public MatchBuilder setTeamB(Team teamB) {
+        this.teamB = teamB;
+        return this;
+    }
+
+    public void setAttributes(Match match) {
+        match.setA(teamA);
+        match.setB(teamB);
+        match.setNumberPlayers(numberPlayers);
+    }
+
+}
---
+++ src/main/java/application/repository/MatchRepository.java
@@ -0,0 +1,9 @@
+package application.repository;
+
+import application.entity.Match;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MatchRepository extends JpaRepository<Match,Long> { // Repositorio que faz a ponte entre a aplicação e o banco de dados da classe Match.
+}
